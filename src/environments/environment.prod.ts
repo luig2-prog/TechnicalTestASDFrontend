948cba7d883a4ab4007ed2d73965a6b3ba390754
luisjimenezh8 <luisjimenezh8@gmail.com>
2022-12-30 16:24:34 -0500

    add compilado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // urlBase:"http://localhost:8082/api",
-  urlBase:"http://ec2-54-224-159-164.compute-1.amazonaws.com:8082/api",
+  urlBase:"http://ec2-54-175-13-10.compute-1.amazonaws.com:8082/api",
   // urlBase:"https://asistenciawebv2-dev.grupokonecta.co:5005/back/api",
   production: true
 };
